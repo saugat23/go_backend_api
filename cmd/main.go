e629e66d9a07d7d4775b3830694468e02125fa61
saugat23 <saugatbhandari8@gmail.com>
2025-02-24 19:13:53 +0545

    db set

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,46 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/saugat23/go_backend_api/cmd/api"
+	"github.com/saugat23/go_backend_api/config"
+	"github.com/saugat23/go_backend_api/db"
 )
 
 func main() {
+
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
 	server := api.NewAPIServer(":8000", nil)
 
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("DB:Successfully Connected")
 }
