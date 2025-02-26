82a5d723a09715e50d99062790e73573c4fe1cd7
saugat23 <saugatbhandari8@gmail.com>
2025-02-26 20:48:18 +0545

    utils and user model init

--- service/user/routes.go
+++ service/user/routes.go
@@ -1,7 +1,11 @@
 package user
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"github.com/saugat23/go_backend_api/types"
+	"github.com/saugat23/go_backend_api/utils"
 
 	"github.com/gorilla/mux"
 )
@@ -23,5 +27,12 @@ func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+	// get JSON payload
+	var payload types.RegisterUserPayload
+	if err := utils.ParseJSON(r, payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+	}
+	//check if user exists
 
+	//if not, create the user
 }
---
+++ types/types.go
@@ -0,0 +1,21 @@
+package types
+
+import (
+	"time"
+)
+
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type RegisterUserPayload struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
